9e27e2db9332336d76a8337ab03a13cf3c1cf983
cryptojessy <96753992+cryptojessy@users.noreply.github.com>
2021-12-28 03:09:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,40 @@ Token =os.environ.get("BOT_TOKEN",None)
 updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
- updater.message.reply_text(f'Hello {member.full_name} , Welcome to ln VIP Thank you for Joining  ')
+ updater.message.reply_text('''
+ 
+ Hello, '{member.full_name}' 👋  👋
+
+Welcome to the VIP community.
+
+This is basic info about a form of Signal you need to know
+
+- Target/ Entry/ SL are clear in  Signal.
+- Trade is opened at Entry
+- Trade is closed at TP
+- Trade is closed at Stop loss
+
+- If there is no update, just follow info given at original signal
+
+- If there is new update something like ( cancel trade, or take profit partially ...etc), follow new update posts.
+
+- When price is moving on right way, It is recommended to move stoploss price to profit zone.
+
+- When price come to 1st Target zone, It is better to take profit one part. let the rest run to profit.
+
+You need to know that you need to spend a maximum of 5% of your money on each signal.
+ 
+ 
+ 
+ 
+ 
+ 
+ Hi iam welcome messanger bot 
+Add me to your group  testvame tuka gluposti
+ 
+ Made with Love ❤️ by @lntechnical
+
+  ''')
  
  
  
